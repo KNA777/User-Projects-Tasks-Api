d05658625543cf77e49e299f134a588ce96a00a8
Nikita <vostrotin.dan@yandex.ru>
2025-09-16 18:45:42 +0300

    some fixies in tests

--- tests/integration_tests/users/test_api.py
+++ tests/integration_tests/users/test_api.py
@@ -1,7 +1,12 @@
-
 async def test_user(auth_ac, db):
     response = await auth_ac["ac_client"].get(
         url="/users/get_me"
+    )
+    assert response.status_code == 200
+    user_id = auth_ac["user"]["id"]
+
+    response = await auth_ac["ac_client"].get(
+        url=f"/users/{user_id}"
     )
 
     assert response.status_code == 200
@@ -11,12 +16,3 @@ async def test_user(auth_ac, db):
     )
     assert response.status_code == 200
     assert "access_token" not in auth_ac["ac_client"].cookies
-
-
-    user_id = auth_ac["user"]["id"]
-
-    response = await auth_ac["ac_client"].get(
-        url=f"/users/{user_id}"
-    )
-
-    assert response.status_code == 200
